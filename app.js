99d71a450049bb13debec6250684bb7048eb2364
Artem <artemivlev@yandex.ru>
2021-05-28 20:22:58 +0300

    cors

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(cors());
+
 const allowedCors = [
   'https://api.mestoivlev.students.nomoredomains.icu',
   'http://api.mestoivlev.students.nomoredomains.icu',
@@ -29,13 +31,13 @@ const allowedCors = [
   'localhost:3000',
 ];
 
-app.use(
-  cors({
-    origin: allowedCors,
-    methods: 'GET,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Origin,Content-Type,Accept',
-  }),
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedCors);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+});
 
 app.use(express.json());
 app.use(requestLogger);
