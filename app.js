5472b2586083533c5cc3a068c4491c767f5a668b
Artem <artemivlev@yandex.ru>
2021-05-28 20:34:28 +0300

    cors

--- app.js
+++ app.js
@@ -14,23 +14,11 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors());
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-});
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  }
-  next();
 });
 
 app.use(express.json());
